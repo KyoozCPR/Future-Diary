f20d04775e7b32f9587a7206eabfff5ee6686410
Diego ciprietti <cipriettid949@gmail.com>
2024-08-14 19:09:29 +0200

    working on anime character section

--- anime/models.py
+++ anime/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.conf import settings
+
 
 class AnimeCharacter(models.Model):
     description = models.TextField(max_length=500)
     name = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="characters/")
+    image_path = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
+    
+    def get_image_url(self):
+        return f'{settings.STATIC_URL}{self.image_path}'
