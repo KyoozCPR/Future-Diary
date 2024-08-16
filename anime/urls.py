14f3861841a812681b84b39a3e55d01dcab19b5f
Diego ciprietti <cipriettid949@gmail.com>
2024-08-17 00:31:20 +0200

    fixed character query problem 🚀

--- anime/urls.py
+++ anime/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="index"),
-    path("character/<int:id>",views.character, name="character")
+    path("character/<int:id>/",views.character, name="character")
 ]
--- anime/views.py
+++ anime/views.py
@@ -5,8 +5,8 @@ from .models import AnimeCharacter
 def home(request): 
     return render(request, "index.html")
 
-def character(request, pk):
-    character = AnimeCharacter.objects.get(id=pk)
+def character(request, id):
+    character = AnimeCharacter.objects.get(id=id)
     return render(request, "character.html", {"character": character})
 
     
