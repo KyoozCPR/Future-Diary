339ddf1d6d5dd24b9d5314892042809f3a5cecbc
Diego ciprietti <cipriettid949@gmail.com>
2024-09-10 02:59:36 +0200

    aggiunto impostazioni production

--- futurediary/settings.py
+++ futurediary/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost','127.0.0.1','futurediary.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://futurediary.up.railway.app', '127.0.0.1']
 
 # Application definition
 
